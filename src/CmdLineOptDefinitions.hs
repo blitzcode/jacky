decf88f22a99ba53457cfab1556ead6577ed421b
Tim C. Schroeder <tim@blitzcode.net>
2013-11-02 19:43:06 +0100

    Moved command line option definitions into separate module

---
+++ src/CmdLineOptDefinitions.hs
@@ -0,0 +1,157 @@
+
+{-# LANGUAGE ScopedTypeVariables, FlexibleContexts #-}
+
+module CmdLineOptDefinitions where -- Export everything
+
+import System.Console.GetOpt
+import System.Environment
+import Control.Monad.IO.Class
+import Control.Monad.Error
+
+-- Command line flag / option parsing, defaults
+
+data Flag = FlagOAuthFile String
+          | FlagHelp
+          | FlagLogNetwork
+          | FlagReplayLog
+          | FlagLogFolder String
+          | FlagImageCacheFolder String
+          | FlagConcImgFetches String
+          | FlagVerifyImgCache
+          | FlagTraceFile String
+          | FlagTraceLevel String
+          | FlagTraceEchoOn
+          | FlagTraceAppend
+          | FlagConKeepAlive String
+          | FlagConTimeout String
+          | FlagTweetHistory String
+          | FlagImgMemCacheSize String
+          | FlagStatTraceInterval String
+          | FlagFirehose
+            deriving (Eq, Show)
+
+defLogFolder, defImageCacheFolder, defTraceFn :: String
+defLogFolder = "./log/"
+defImageCacheFolder = "http_img_cache"
+defTraceFn = "./trace.log"
+defConcImgFetches :: Int
+defConcImgFetches = 50
+defConKeepAlive :: Int
+defConKeepAlive = 10
+defConTimeout :: Int
+defConTimeout = 5 * 1000 * 1000
+defTweetHistory :: Int
+defTweetHistory = 1000
+defImgMemCacheSize :: Int
+defImgMemCacheSize = 1024
+defStatTraceInterval = 10.0
+defStatTraceInterval :: Double
+
+parseCmdLineOpt :: (MonadError String m, MonadIO m) => m [Flag]
+parseCmdLineOpt = do
+    name <- liftIO $ getProgName
+    args <- liftIO $ getArgs
+    let header = "Usage: " ++ name ++ " [OPTION...]"
+        usage  = usageInfo header options
+    flags <- case getOpt Permute options args of
+                 (f , [], [] ) -> return f
+                 (_ , _ , err) -> throwError ("\n" ++ concat err ++ usage) >> return []
+    when (FlagHelp `elem` flags) $ (liftIO $ putStrLn usage) >> throwError "Done, exiting"
+    return flags
+    where
+        options :: [OptDescr Flag]
+        options = [ Option ['o']
+                           ["oauth"]
+                           (ReqArg FlagOAuthFile "FILE")
+                           (  "load OAuth details from FILE\n"
+                           ++ "expected format / contents:\n"
+                           ++ "  oauth_consumer_key    = ...\n"
+                           ++ "  oauth_consumer_secret = ...\n"
+                           ++ "  oauth_token           = ...\n"
+                           ++ "  oauth_token_secret    = ...\n"
+                           ++ "see https://dev.twitter.com/apps/new"
+                           )
+                  , Option []
+                           ["firehose"]
+                           (NoArg FlagFirehose)
+                           "connect to firehose instead of user home timeline"
+                  , Option ['f']
+                           ["log-folder"]
+                           (ReqArg FlagLogFolder "FOLDER")
+                           ("location of network data dump (default: " ++ defLogFolder ++ ")")
+                  , Option ['l']
+                           ["log-network"]
+                           (NoArg FlagLogNetwork)
+                           "dump all received API network data in log folder"
+                  , Option ['r']
+                           ["replay-log"]
+                           (NoArg FlagReplayLog)
+                           "replay dumped API network data from log folder"
+                  , Option ['i']
+                           ["http-imgcache-folder"]
+                           (ReqArg FlagImageCacheFolder "FOLDER")
+                           ("image cache fldr (default: '"
+                               ++ defImageCacheFolder ++ "' in HOME)")
+                  , Option []
+                           ["conc-img-fetches"]
+                           (ReqArg FlagConcImgFetches "NUMBER")
+                           ("number of concurrent image fetches (default: "
+                               ++ show defConcImgFetches ++ ")")
+                  , Option ['m']
+                           ["img-mem-cache-size"]
+                           (ReqArg FlagImgMemCacheSize "NUMBER")
+                           ("number of images to keep in mem cache (default: "
+                               ++ show defImgMemCacheSize ++ ")")
+                  , Option []
+                           ["conn-keep-alive"]
+                           (ReqArg FlagConKeepAlive "NUMBER")
+                           ("# of conn. to single host to keep alive (default: "
+                               ++ show defConKeepAlive ++ ")")
+                  , Option []
+                           ["conn-timeout"]
+                           (ReqArg FlagConTimeout "NUMBER")
+                           ("HTTP request timeout (in µs, default: "
+                               ++ show defConTimeout ++ ")")
+                  , Option ['s']
+                           ["stat-trace-interval"]
+                           (ReqArg FlagStatTraceInterval "NUMBER")
+                           ("statistics tracing interval (in seconds, default: "
+                               ++ show defStatTraceInterval ++ ")")
+                  , Option ['n']
+                           ["tweet-hist"]
+                           (ReqArg FlagTweetHistory "NUMBER")
+                           ("number of tweets to keep around (default: "
+                               ++ show defTweetHistory ++ ")")
+                  , Option []
+                           ["trace-file"]
+                           (ReqArg FlagTraceFile "FILE")
+                           ("output file for execution trace (default: " ++ defTraceFn ++ ")")
+                  , Option ['t']
+                           ["trace-level"]
+                           (ReqArg FlagTraceLevel "LEVEL")
+                           (  "execution trace level (default: n)\n"
+                           ++ "  n = none\n"
+                           ++ "  e = errors only\n"
+                           ++ "  w = warnings and errors\n"
+                           ++ "  i = infos, warnings and errors"
+                           )
+                  , Option ['e']
+                           ["trace-echo"]
+                           (NoArg FlagTraceEchoOn)
+                           ("echo execution trace to stdout as well")
+                  , Option []
+                           ["trace-append"]
+                           (NoArg FlagTraceAppend)
+                           ("append execution trace file instead of overwriting")
+                  , Option []
+                           ["verify-img-cache"]
+                           (NoArg FlagVerifyImgCache)
+                           "debug: try to read & decode all cached images"
+                  , Option ['h']
+                           ["help"]
+                           (NoArg FlagHelp)
+                           "print usage information"
+                  -- TODO: Option for offline mode
+                  -- TODO: Option to discard caches
+                  ]
+
--- src/Main.hs
+++ src/Main.hs
@@ -7,9 +7,7 @@
 
 module Main where
 
-import System.Environment (getArgs, getProgName)
 import System.Exit
-import System.Console.GetOpt
 import Control.Applicative
 import qualified Data.Map.Strict as M
 import Control.Monad.Error
@@ -45,6 +43,7 @@ import GLHelpers
 import TextureCache
 import qualified RectPacker as RP
 import BoundedSequence as BS
+import CmdLineOptDefinitions
 
 -- TODO: Start using Lens library for records and Reader/State
 -- TODO: Use labelThread for all threads
@@ -79,172 +78,6 @@ data State = State
     }
 
 type AppDraw = RWST Env () State IO
-
-data Flag = FlagOAuthFile String
-          | FlagHelp
-          | FlagLogNetwork
-          | FlagReplayLog
-          | FlagLogFolder String
-          | FlagImageCacheFolder String
-          | FlagConcImgFetches String
-          | FlagVerifyImgCache
-          | FlagTraceFile String
-          | FlagTraceLevel String
-          | FlagTraceEchoOn
-          | FlagTraceAppend
-          | FlagConKeepAlive String
-          | FlagConTimeout String
-          | FlagTweetHistory String
-          | FlagImgMemCacheSize String
-          | FlagStatTraceInterval String
-          | FlagFirehose
-            deriving (Eq, Show)
-
-defLogFolder, defImageCacheFolder, defTraceFn :: String
-defLogFolder = "./log/"
-defImageCacheFolder = "http_img_cache"
-defTraceFn = "./trace.log"
-defConcImgFetches :: Int
-defConcImgFetches = 50
-defConKeepAlive :: Int
-defConKeepAlive = 10
-defConTimeout :: Int
-defConTimeout = 5 * 1000 * 1000
-defTweetHistory :: Int
-defTweetHistory = 1000
-defImgMemCacheSize :: Int
-defImgMemCacheSize = 1024
-defStatTraceInterval = 10.0
-defStatTraceInterval :: Double
-
-parseCmdLineOpt :: (MonadError String m, MonadIO m) => m [Flag]
-parseCmdLineOpt = do
-    name <- liftIO $ getProgName
-    args <- liftIO $ getArgs
-    let header = "Usage: " ++ name ++ " [OPTION...]"
-        usage  = usageInfo header options
-    flags <- case getOpt Permute options args of
-                 (f , [], [] ) -> return f
-                 (_ , _ , err) -> throwError ("\n" ++ concat err ++ usage) >> return []
-    when (FlagHelp `elem` flags) $ (liftIO $ putStrLn usage) >> throwError "Done, exiting"
-    return flags
-    where
-        options :: [OptDescr Flag]
-        options = [ Option ['o']
-                           ["oauth"]
-                           (ReqArg FlagOAuthFile "FILE")
-                           (  "load OAuth details from FILE\n"
-                           ++ "expected format / contents:\n"
-                           ++ "  oauth_consumer_key    = ...\n"
-                           ++ "  oauth_consumer_secret = ...\n"
-                           ++ "  oauth_token           = ...\n"
-                           ++ "  oauth_token_secret    = ...\n"
-                           ++ "see https://dev.twitter.com/apps/new"
-                           )
-                  , Option []
-                           ["firehose"]
-                           (NoArg FlagFirehose)
-                           "connect to firehose instead of user home timeline"
-                  , Option ['f']
-                           ["log-folder"]
-                           (ReqArg FlagLogFolder "FOLDER")
-                           ("location of network data dump (default: " ++ defLogFolder ++ ")")
-                  , Option ['l']
-                           ["log-network"]
-                           (NoArg FlagLogNetwork)
-                           "dump all received API network data in log folder"
-                  , Option ['r']
-                           ["replay-log"]
-                           (NoArg FlagReplayLog)
-                           "replay dumped API network data from log folder"
-                  , Option ['i']
-                           ["http-imgcache-folder"]
-                           (ReqArg FlagImageCacheFolder "FOLDER")
-                           ("image cache fldr (default: '"
-                               ++ defImageCacheFolder ++ "' in HOME)")
-                  , Option []
-                           ["conc-img-fetches"]
-                           (ReqArg FlagConcImgFetches "NUMBER")
-                           ("number of concurrent image fetches (default: "
-                               ++ show defConcImgFetches ++ ")")
-                  , Option ['m']
-                           ["img-mem-cache-size"]
-                           (ReqArg FlagImgMemCacheSize "NUMBER")
-                           ("number of images to keep in mem cache (default: "
-                               ++ show defImgMemCacheSize ++ ")")
-                  , Option []
-                           ["conn-keep-alive"]
-                           (ReqArg FlagConKeepAlive "NUMBER")
-                           ("# of conn. to single host to keep alive (default: "
-                               ++ show defConKeepAlive ++ ")")
-                  , Option []
-                           ["conn-timeout"]
-                           (ReqArg FlagConTimeout "NUMBER")
-                           ("HTTP request timeout (in µs, default: "
-                               ++ show defConTimeout ++ ")")
-                  , Option ['s']
-                           ["stat-trace-interval"]
-                           (ReqArg FlagStatTraceInterval "NUMBER")
-                           ("statistics tracing interval (in seconds, default: "
-                               ++ show defStatTraceInterval ++ ")")
-                  , Option ['n']
-                           ["tweet-hist"]
-                           (ReqArg FlagTweetHistory "NUMBER")
-                           ("number of tweets to keep around (default: "
-                               ++ show defTweetHistory ++ ")")
-                  , Option []
-                           ["trace-file"]
-                           (ReqArg FlagTraceFile "FILE")
-                           ("output file for execution trace (default: " ++ defTraceFn ++ ")")
-                  , Option ['t']
-                           ["trace-level"]
-                           (ReqArg FlagTraceLevel "LEVEL")
-                           (  "execution trace level (default: n)\n"
-                           ++ "  n = none\n"
-                           ++ "  e = errors only\n"
-                           ++ "  w = warnings and errors\n"
-                           ++ "  i = infos, warnings and errors"
-                           )
-                  , Option ['e']
-                           ["trace-echo"]
-                           (NoArg FlagTraceEchoOn)
-                           ("echo execution trace to stdout as well")
-                  , Option []
-                           ["trace-append"]
-                           (NoArg FlagTraceAppend)
-                           ("append execution trace file instead of overwriting")
-                  , Option []
-                           ["verify-img-cache"]
-                           (NoArg FlagVerifyImgCache)
-                           "debug: try to read & decode all cached images"
-                  , Option ['h']
-                           ["help"]
-                           (NoArg FlagHelp)
-                           "print usage information"
-                  -- TODO: Option for offline mode
-                  -- TODO: Option to discard caches
-                  ]
-
-setupOAuth :: (MonadError String m, MonadIO m) => FilePath -> m (OA.OAuth, OA.Credential)
-setupOAuth fn = do
-    -- OAuth configuration file
-    oauthCfg <- liftIO $ (loadCfgFile fn)
-                `catch` (\(e :: IOException) -> print e >> return M.empty)
-    when (M.null oauthCfg) $ throwError "Invalid OAuth configuration file"
-    -- OAuth client and credentials
-    let oaClient = OA.newOAuth
-            { OA.oauthConsumerKey    = B8.pack $
-                  M.findWithDefault "<NoKey>"    "oauth_consumer_key"    oauthCfg
-            , OA.oauthConsumerSecret = B8.pack $
-                  M.findWithDefault "<NoSecret>" "oauth_consumer_secret" oauthCfg
-            }
-        oaCredential = OA.newCredential
-            (B8.pack $ M.findWithDefault "<NoToken>"  "oauth_token"        oauthCfg)
-            (B8.pack $ M.findWithDefault "<NoSecret>" "oauth_token_secret" oauthCfg)
-    return (oaClient, oaCredential)
-
-runOnAllCores :: IO ()
-runOnAllCores = GHC.Conc.getNumProcessors >>= setNumCapabilities
 
 
 color3f :: Float -> Float -> Float -> IO ()
@@ -539,6 +372,27 @@ verifyImgCache folder = do
                 Left err -> putStrLn $ "\n" ++ fn ++ ": " ++ err
                 Right _  -> putStr "." >> hFlush stdout
     putStrLn ""
+
+runOnAllCores :: IO ()
+runOnAllCores = GHC.Conc.getNumProcessors >>= setNumCapabilities
+
+setupOAuth :: (MonadError String m, MonadIO m) => FilePath -> m (OA.OAuth, OA.Credential)
+setupOAuth fn = do
+    -- OAuth configuration file
+    oauthCfg <- liftIO $ (loadCfgFile fn)
+                `catch` (\(e :: IOException) -> print e >> return M.empty)
+    when (M.null oauthCfg) $ throwError "Invalid OAuth configuration file"
+    -- OAuth client and credentials
+    let oaClient = OA.newOAuth
+            { OA.oauthConsumerKey    = B8.pack $
+                  M.findWithDefault "<NoKey>"    "oauth_consumer_key"    oauthCfg
+            , OA.oauthConsumerSecret = B8.pack $
+                  M.findWithDefault "<NoSecret>" "oauth_consumer_secret" oauthCfg
+            }
+        oaCredential = OA.newCredential
+            (B8.pack $ M.findWithDefault "<NoToken>"  "oauth_token"        oauthCfg)
+            (B8.pack $ M.findWithDefault "<NoSecret>" "oauth_token_secret" oauthCfg)
+    return (oaClient, oaCredential)
 
 main :: IO ()
 main = do
